58340dd93acb6058543f6f114dd40a6ae8fa58f8
WIN_NT\x15g017 <x15g017@chiba-fjb.ac.jp>
2017-05-01 12:38:57 +0900

    コントロールの扱い

---
+++ app/src/main/java/jp/ac/chiba_fjb/x15g017/test08/MainActivity.java
@@ -0,0 +1,35 @@
+package jp.ac.chiba_fjb.x15g017.test08;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(""+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
